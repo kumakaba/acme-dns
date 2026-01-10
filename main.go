d296f33e88a04b9d92a84a5a6f70dd715c391896
kumakaba <kmkb@rtclub.net>
2026-01-10 15:58:08 +0900

    Release v1.3.3

--- main.go
+++ main.go
@@ -19,8 +19,8 @@ import (
 )
 
 var (
-	Version  = "v1.3.3"           // VERSION_STR
-	Revision = "preview20260106a" // VERSION_STR
+	Version  = "v1.3.3"  // VERSION_STR
+	Revision = "release" // VERSION_STR
 )
 
 func main() {
